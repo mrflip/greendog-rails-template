3b6069e9720501561a054c06c754e1023668ea56
Philip (flip) Kromer <flip@infochimps.org>
2012-01-03 02:16:12 -0600

    app layout yay

--- _boilerplate.rb
+++ _boilerplate.rb
@@ -9,6 +9,8 @@ copy_static_file 'app/assets/stylesheets/bootstrap.sass'
 copy_static_file 'config/compass.rb'
 copy_static_file 'app/views/misc/bootstrap_demo.html.erb'
 
+gsub_file( 'app/assets/javascripts/application.js', /.*require jquery$/, '')
+
 inject_into_file 'config/application.rb', :after => "config.assets.enabled = true\n" do
 <<-RUBY
     # Compass integration
@@ -17,31 +19,38 @@ inject_into_file 'config/application.rb', :after => "config.assets.enabled = tru
 RUBY
 end
 
+
 %w[
   bootstrap/bootstrap-dropdown.js
   bootstrap/bootstrap-scrollspy.js
   bootstrap/bootstrap-twipsy.js
   bootstrap/demo/bootstrap_demo.css
   bootstrap/demo/bootstrap_demo.js
-  bootstrap/demo/ico/bootstrap-apple-114x114.png
-  bootstrap/demo/ico/bootstrap-apple-57x57.png
-  bootstrap/demo/ico/bootstrap-apple-72x72.png
-  bootstrap/demo/images/bird.png
-  bootstrap/demo/images/browsers.png
-  bootstrap/demo/images/example-diagram-01.png
-  bootstrap/demo/images/example-diagram-02.png
-  bootstrap/demo/images/example-diagram-03.png
-  bootstrap/demo/images/grid-18px.png
+  bootstrap/demo/grid-18px.png
   google-code-prettify/prettify.css
   google-code-prettify/prettify.js
-  jquery.touch_events.js
   jquery.tablesorter.js
-  jquery.bbq.js
+].each{|asset| copy_static_file("public/assets/#{asset}")}
+git :add => '.'
+git :commit => "-am 'Twitter Bootstrap Helpers.'"
+
+%w[
   hashgrid.js
+].each{|asset| copy_static_file("public/assets/#{asset}")}
+git :add => '.'
+git :commit => "-am 'Hashgrid grid visualizer.'"
+
+%w[
+  jquery.bbq.js
+  jquery.touch_events.js
   sprintf.js
+].each{|asset| copy_static_file("public/assets/#{asset}")}
+git :add => '.'
+git :commit => "-am 'jQuery helpers.'"
+
+%w[
   coffee-script.js
   underscore.coffee
 ].each{|asset| copy_static_file("public/assets/#{asset}")}
-
 git :add => '.'
-git :commit => "-am 'Generated boilerplate.'"
+git :commit => "-am 'javascript utils.'"
